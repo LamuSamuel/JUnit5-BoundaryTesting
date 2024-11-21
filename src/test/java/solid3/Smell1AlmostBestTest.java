e1d5b15ad33faaa93aacc985c42773925737a738
Lamu Samuel <lamusamuel@Samuels-MacBook-Pro.local>
2024-11-20 22:24:10 -0700

    unit test case three base one raised to larger value should return one

--- src/test/java/solid3/Smell1AlmostBestTest.java
+++ src/test/java/solid3/Smell1AlmostBestTest.java
@@ -36,8 +36,19 @@ class Smell1AlmostBestTest {
         int[] expected = {1, 1, 1};
         assertArrayEquals(expected, result);
 
-
-
+    }
+    @Test
+    @DisplayName("base one raise to greater power should return one")
+    void baseOneRaisesToGreaterPower() {
+        Smell1AlmostBest power = new Smell1AlmostBest();
+        int[] exponents = {99, 19999,199999};
+        int base = 1;
+        int[] result = new int[exponents.length];
+        for (int i = 0; i < exponents.length; i++) {
+            result[i] = power.toPower(base, exponents[i]);
+        }
+        int[] expected = {1, 1, 1};
+        assertArrayEquals(expected, result);
     }
 
 }
