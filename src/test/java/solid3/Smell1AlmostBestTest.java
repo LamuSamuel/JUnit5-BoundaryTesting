c5c7369470ef7cc187b01cf95363611b5f56d0c6
Lamu Samuel <lamusamuel@Samuels-MacBook-Pro.local>
2024-11-20 22:09:31 -0700

    unit case two Positive base raised to Exponent Zero

--- src/test/java/solid3/Smell1AlmostBestTest.java
+++ src/test/java/solid3/Smell1AlmostBestTest.java
@@ -8,18 +8,36 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 class Smell1AlmostBestTest {
 
     @Test
-    @DisplayName("Positive base raised to positive power")
-    void PositiveBaseToPositivePower()  {
+    @DisplayName("Positive base raised to Positive Exponent")
+    void PositiveBaseToPositivePower() {
         Smell1AlmostBest power = new Smell1AlmostBest();
         int[] exponents = {3, 4, 5};
         int base = 2;
         int[] result = new int[exponents.length];
-        for (int i=0;i< exponents.length;i++)
-        {
+        for (int i = 0; i < exponents.length; i++) {
             result[i] = power.toPower(base, exponents[i]);
 
         }
-        int [] expected = {8,16,32};
-        assertArrayEquals(expected,result);
+        int[] expected = {8, 16, 32};
+        assertArrayEquals(expected, result);
     }
+
+    @Test
+    @DisplayName("Positive base raised to Exponent Zero")
+    void PositiveBaseToExponentZero() {
+        Smell1AlmostBest power = new Smell1AlmostBest();
+        int[] exponents = {0, 0, 0};
+        int base = 2;
+        int[] result = new int[exponents.length];
+        for (int i = 0; i < exponents.length; i++) {
+            result[i] = power.toPower(base, exponents[i]);
+
+        }
+        int[] expected = {1, 1, 1};
+        assertArrayEquals(expected, result);
+
+
+
+    }
+
 }
