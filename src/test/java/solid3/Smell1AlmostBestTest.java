209266296a9cc81902d3f5365aa6d0165a6b96d4
Darryl Saldanha <dsalda11@asu.edu>
2024-11-20 23:03:05 -0700

    base zero raised to a positive number

--- src/test/java/solid3/Smell1AlmostBestTest.java
+++ src/test/java/solid3/Smell1AlmostBestTest.java
@@ -1,8 +1,30 @@
 package solid3;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.DisplayName;
 
-class Smell1AmostBestTest {
+
+class Smell1AlmostBestTest {
+
+    Smell1AlmostBest power  = new Smell1AlmostBest();
+
+    @Test
+    @DisplayName("Raising Zero to a positive")
+    public void BaseZeroPositiveExponent(){
+
+        int[] exponents = {2,3,4};
+        int[] expectedVal = {0,0,0};
+        int[] result = new int[expectedVal.length];
+        int base = 0;
+
+        for(int i =0;i<expectedVal.length;i++)
+        {
+            result[i] = power.toPower(base, exponents[i]);
+        }
+        assertArrayEquals(expectedVal,result);
+    }
 
 }
