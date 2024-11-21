7206eba6ce1055c34b43f5fd6302a94a3870ad24
Lamu Samuel <lamusamuel@Samuels-MacBook-Pro.local>
2024-11-20 21:58:52 -0700

    testing func PositiveBaseToPositivePower

--- src/test/java/solid3/Smell1AlmostBestTest.java
+++ src/test/java/solid3/Smell1AlmostBestTest.java
@@ -1,8 +1,25 @@
 package solid3;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 
-class Smell1AmostBestTest {
+class Smell1AlmostBestTest {
 
+    @Test
+    @DisplayName("Positive base raised to positive power")
+    void PositiveBaseToPositivePower()  {
+        Smell1AlmostBest power = new Smell1AlmostBest();
+        int[] exponents = {3, 4, 5};
+        int base = 2;
+        int[] result = new int[exponents.length];
+        for (int i=0;i< exponents.length;i++)
+        {
+            result[i] = power.toPower(base, exponents[i]);
+
+        }
+        int [] expected = {8,16,32};
+        assertArrayEquals(expected,result);
+    }
 }
